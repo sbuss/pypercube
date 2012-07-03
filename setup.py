5d6f6ec4ad5015d122f6287aef6f74e621ebc19b
Steven Buss <steven.buss@gmail.com>
2012-07-03 13:33:26 -0700

    Add setup.py and init package.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+from pypercube import __version__
+
+setup(
+        name='pypercube',
+        version=__version__,
+        description='A Cube API client in python.',
+        long_description="An easy way to use Cube's API in python",
+        author='Steven Buss',
+        author_email='steven.buss@gmail.com',
+        url='https://github.com/sbuss/pypercube',
+        download_url='https://github.com/sbuss/pypercube/'\
+                'tarball/v%s' % __version__,
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: System :: Logging",
+        ],
+        packages=[
+            'pypercube',
+        ],
+)
