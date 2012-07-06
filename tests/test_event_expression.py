3c0792018a030d88216ba47f9ea2f535bb7f8b41
Steven Buss <steven.buss@gmail.com>
2012-07-06 13:09:52 -0700

    Test EventExpression filters.

--- tests/test_event_expression.py
+++ tests/test_event_expression.py
@@ -70,3 +70,58 @@ class TestEventExpressions(unittest.TestCase):
         e2 = EventExpression('request', ['path', 'elapsed_ms']).eq(
             'path', '/').gt('elapsed_ms', 500)
         self.assertEqual(e1, e2)
+
+    def _test_filter(self, filter_str):
+        e = EventExpression('test')
+        self.assertTrue(hasattr(e, filter_str))
+        f = getattr(e, filter_str)
+        filtered = f('elapsed_ms', 500)
+        self.assertTrue(isinstance(filtered, EventExpression))
+        self.assertTrue(len(filtered.filters), 1)
+        self.assertEqual("%s" % filtered,
+                "test.{filter_str}(elapsed_ms, 500)".format(
+                    filter_str=filter_str))
+
+    def test_eq(self):
+        self._test_filter('eq')
+
+    def test_lt(self):
+        self._test_filter('lt')
+
+    def test_le(self):
+        self._test_filter('le')
+
+    def test_gt(self):
+        self._test_filter('gt')
+
+    def test_ge(self):
+        self._test_filter('ge')
+
+    def test_ne(self):
+        self._test_filter('ne')
+
+    def test_re(self):
+        self._test_filter('re')
+
+    def test_startswith(self):
+        e = EventExpression('test')
+        f = e.startswith('foo', 'bar')
+        self.assertEqual("%s" % f, 'test.re(foo, "^bar")')
+
+    def test_endswith(self):
+        e = EventExpression('test')
+        f = e.endswith('foo', 'bar')
+        self.assertEqual("%s" % f, 'test.re(foo, ".*bar$")')
+
+    def test_contains(self):
+        e = EventExpression('test')
+        f = e.contains('foo', 'bar')
+        self.assertEqual("%s" % f, 'test.re(foo, ".*bar.*")')
+
+    def test_in_array(self):
+        e = EventExpression('test')
+        f = e.in_array('foo', ['bar', 'baz'])
+        self.assertEqual("%s" % f, 'test.in(foo, ["bar", "baz"])')
+
+        f = e.in_array('foo', 'bar')
+        self.assertEqual("%s" % f, 'test.in(foo, ["b", "a", "r"])')
