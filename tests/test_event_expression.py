c943ca087be475e5dcde7a3e6d2981a43b2563a2
Steven Buss <steven.buss@gmail.com>
2012-07-06 14:22:29 -0700

    Add cube & query tests, move mock queries to Cube test.

--- tests/test_event_expression.py
+++ tests/test_event_expression.py
@@ -1,42 +1,12 @@
-from datetime import datetime
-import json
 import unittest
 
 from pypercube.cube import Cube
-from pypercube.cube import Query
-from pypercube.event import Event
 from pypercube.expression import EventExpression
-
-from tests import MockResponse
-from tests import mock_get
 
 
 class TestEventExpressions(unittest.TestCase):
     def setUp(self):
         self.c = Cube('unittest')
-
-    def test_no_matching_events(self):
-        mock_response = MockResponse(ok=True, status_code='200',
-                content="[]", json=[])
-        Query.get = mock_get(mock_response)
-
-        event = EventExpression('test')
-        response = self.c.get_event(event, limit=10)
-        self.assertEqual(len(response), 0)
-
-    def test_single_matching_event(self):
-        timestamp = datetime.utcnow()
-        expected_content = '[{"time":"' + timestamp.isoformat() + '"}]'
-
-        mock_response = MockResponse(ok=True, status_code='200',
-                content=expected_content, json=json.loads(expected_content))
-        Query.get = mock_get(mock_response)
-
-        event = EventExpression('test')
-        response = self.c.get_event(event, limit=1)
-        self.assertEqual(len(response), 1)
-        self.assertTrue(isinstance(response[0], Event))
-        self.assertEqual(response[0].time, timestamp)
 
     def test_copy(self):
         e1 = EventExpression('request', ['path', 'elapsed_ms'])
--- tests/test_metric_expression.py
+++ tests/test_metric_expression.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-import json
 import unittest
 
 from pypercube.cube import Cube
-from pypercube.cube import Query
 from pypercube.expression import Distinct
 from pypercube.expression import EventExpression
 from pypercube.expression import Max
@@ -11,43 +8,12 @@ from pypercube.expression import Median
 from pypercube.expression import MetricExpression
 from pypercube.expression import Min
 from pypercube.expression import Sum
-from pypercube.metric import Metric
-
-from tests import MockResponse
-from tests import mock_get
 
 
 class TestMetricExpressions(unittest.TestCase):
     def setUp(self):
         self.c = Cube('unittest')
         self.e = EventExpression('request')
-
-    def test_no_matching_metrics(self):
-        mock_response = MockResponse(ok=True, status_code='200',
-                content="[]", json=[])
-        Query.get = mock_get(mock_response)
-
-        event = EventExpression('test')
-        metric = Sum(event)
-        response = self.c.get_metric(metric, limit=10)
-        self.assertEqual(len(response), 0)
-
-    def test_single_matching_metric(self):
-        timestamp = datetime.utcnow()
-        expected_content = '[{"time":"' + timestamp.isoformat() + '", '\
-                '"value":100}]'
-
-        mock_response = MockResponse(ok=True, status_code='200',
-                content=expected_content, json=json.loads(expected_content))
-        Query.get = mock_get(mock_response)
-
-        event = EventExpression('test')
-        metric = Sum(event)
-        response = self.c.get_metric(metric, limit=1)
-        self.assertEqual(len(response), 1)
-        self.assertTrue(isinstance(response[0], Metric))
-        self.assertEqual(response[0].time, timestamp)
-        self.assertEqual(response[0].value, 100)
 
     def _test_op(self, op_str, op):
         m1 = MetricExpression(op_str, self.e)
